0269b8f534ec098a9d108f1c051e1d0b19338837
Ubuntu <ubuntu@ip-172-31-31-84.ec2.internal>
2020-03-24 17:47:30 +0000

    Added base html and template pages, basic functionality added

--- app.py
+++ app.py
@@ -12,10 +12,47 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/my_recipes')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_shopping')
+def get_shopping():
+    return render_template("shoppinglist.html", shoppinglist=mongo.db.shoppinglist.find())
+    
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipe=mongo.db.recipes.find())
+    
+@app.route('/add_item')
+def add_item():
+    return render_template("additem.html")
+    
+@app.route('/insert_item', methods=['POST'])
+def insert_item():
+    items = mongo.db.shoppinglist
+    items.insert_one(request.form.to_dict())
+    return redirect(url_for('get_shopping'))
+    
+@app.route('/edit_item/<item_id>')
+def edit_item(item_id):
+    the_item = mongo.db.shoppinglist.find_one({"_id": ObjectId(item_id)})
+    return render_template("edititem.html", item=the_item)
+    
+@app.route('/delete_item/<item_id>')
+def delete_item(item_id):
+    mongo.db.shoppinglist.remove({'_id':ObjectId(item_id)})
+    return redirect(url_for('get_shopping'))
+    
+@app.route('/update_item/<item_id>', methods=["POST"])
+def update_item(item_id):
+    items = mongo.db.shoppinglist
+    items.update( {'_id': ObjectId(item_id)},
+    {
+        'item_name':request.form.get('item_name'),
+        'item_quantity':request.form.get('item_quantity'),
+        'item_info': request.form.get('item_info')
+    })
+    return redirect(url_for('get_shopping'))
 
+
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
