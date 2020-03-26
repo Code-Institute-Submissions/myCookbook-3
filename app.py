5026386954ede24af16fbf419801adcea76f9b7e
Ubuntu <ubuntu@ip-172-31-31-84.ec2.internal>
2020-03-26 13:36:16 +0000

    added add_recipe page

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def add_item():
     
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template("addrecipe.html")
+    return render_template("add_recipe.html")
     
 @app.route('/insert_item', methods=['POST'])
 def insert_item():
